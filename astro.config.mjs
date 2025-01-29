66d9596a4ae3e03e18af79e3fe8f1b3dc4d92059
Dabinci1337 <david@3psychos.de>
2025-01-29 19:18:33 +0100

    fix astro.config

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,5 +6,6 @@ import tailwind from '@astrojs/tailwind';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://ydavinci.github.io/',
+  output: 'static',
   integrations: [tailwind()]
 });
