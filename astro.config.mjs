15790f99aee7cb9443c7c19cb8d091313e459164
David <104783680+ydavinci@users.noreply.github.com>
2025-02-06 20:10:19 +0100

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://ydavinci.github.io/',
+  site: 'https://ydavinci.github.io/desktop.ydavinci.github.io/',
   output: 'static',
   integrations: [tailwind()]
 });
